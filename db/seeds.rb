674177db72ec155477172e520d9cf824fc2bf05f
Satomi Yamanaka <nui.mea5en@gmail.com>
2016-12-11 06:11:31 +0000

    Add seed datas and view for user_name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-                password: password,
-                password_confirmation: password,
-                )
+10.times do |n|
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(email: email,
+#                 password: password,
+#                 password_confirmation: password,
+#                 name: "user #{n}"
+#                 )
+
+  Blog.create(title: "title #{n+10}",
+               content: "content #{n+10}",
+               user_id: 203,
+               )
 end
