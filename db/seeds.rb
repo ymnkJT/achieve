4a1f6c3ddb9a14538a0ad8c4ff795a08165acd87
Satomi Yamanaka <nui.mea5en@gmail.com>
2016-12-11 06:26:56 +0000

    Change seed data for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 10.times do |n|
-#   email = Faker::Internet.email
-#   password = "password"
-#   User.create!(email: email,
-#                 password: password,
-#                 password_confirmation: password,
-#                 name: "user #{n}"
-#                 )
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+                password: password,
+                password_confirmation: password,
+                name: "user #{n}"
+                )
 
   Blog.create(title: "title #{n+10}",
                content: "content #{n+10}",
-               user_id: 203,
+               user_id: 1,
                )
 end
