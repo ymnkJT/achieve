a370d9c7bb7f4b792fe1d686253e8e8df8908a56
ymnkJT <javadtomi@gmail.com>
2016-12-24 22:21:53 +0900

    Set routing of show

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :poems, only: [:index]
+  resources :poems, only: [:index, :show]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
