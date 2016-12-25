b91586c5d6a2da142eedfa9ad69d16ad912952a0
ymnkJT <javadtomi@gmail.com>
2016-12-25 19:53:31 +0900

    Fix spell at devise.rb and add columns for omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,6 +18,6 @@ Devise.setup do |config|
     config.omniauth :twitter, ENV["TWITTER_ID_PRODUCTION"], ENV["TWITTER_SECRET_PRODUCTION"], scope: 'email', display: 'popup', info_fields: 'name, email'
   else
     config.omniauth :facebook, ENV["FACEBOOK_ID_DEVEROPMENT"], ENV["FACEBOOK_SECRET_DEVELOPMENT"], scope: 'email', display: 'popup', info_fields: 'name, email'
-    congig.omniauth :twitter, ENV["TWITTER_ID_DEVELOPMENT"], ENV["TWITTER_SECRET_DEVELOPMENT"], scope: 'email', display: 'popup', info_fields: 'name, email'
+    config.omniauth :twitter, ENV["TWITTER_ID_DEVELOPMENT"], ENV["TWITTER_SECRET_DEVELOPMENT"], scope: 'email', display: 'popup', info_fields: 'name, email'
   end
 end
