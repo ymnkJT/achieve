74123120fb15b31d676381a7eabde7fc1a99d8ca
ymnkJT <javadtomi@gmail.com>
2016-12-29 23:47:33 +0900

    Create image upload function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def profile_img(user)
+    unless user.provider.blank?
+      img_url = user.image_url
+    else
+      img_url = 'no_image.png'
+    end
+    image_tag(img_url, alt: user.name)
+  end
 end
 
 module ActionView
