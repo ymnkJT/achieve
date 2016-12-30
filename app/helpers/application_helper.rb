a4ee56c755a95debca0c74878ca3389a79365247
ymnkJT <javadtomi@gmail.com>
2016-12-31 00:52:30 +0900

    Delete the unwanted blank line

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def profile_img(user)
     return image_tag(user.avatar, alt: user.name) if user.avatar?
-
     unless user.provider.blank?
       img_url = user.image_url
     else
