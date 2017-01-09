1acc0a61f550e7d8bb584c6d89ea7e1d86a914a3
ymnkJT <javadtomi@gmail.com>
2017-01-09 16:14:22 +0900

    Add process to response js format to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,11 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.json { render :show, status: :create, location: @comment }
+        format.js   { render :index }
       else
         format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprossable_entily }
       end
     end
   end
