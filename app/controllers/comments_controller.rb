0aa2152ed7160c91466e29843eaf50fea7b34bb6
ymnkJT <javadtomi@gmail.com>
2017-01-09 23:31:41 +0900

    Edit to implement destroy function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,18 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-        format.json { render :show, status: :create, location: @comment }
         format.js   { render :index }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprossable_entily }
       end
     end
+  end
+
+  def destroy
+    @comment = Comments.find(params[:id])
+    @comment.destroy
+    render js: { status: 'delete', data: @comment }
   end
 
   private
