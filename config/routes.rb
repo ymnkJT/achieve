397538b5a512fb07b527e6064e3fc76fb11af1a3
ymnkJT <javadtomi@gmail.com>
2017-02-04 18:21:01 +0900

    Remove routes created from relationships controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-
-  get 'relationships/destroy'
-
   root 'top#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
