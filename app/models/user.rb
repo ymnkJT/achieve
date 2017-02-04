f6e38fb45af70512b211ca6d507b93d5bec35be0
ymnkJT <javadtomi@gmail.com>
2017-02-04 20:21:58 +0900

    Add method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,4 +57,12 @@ class User < ActiveRecord::Base
       update_without_password(params, *options)
     end
   end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
 end
