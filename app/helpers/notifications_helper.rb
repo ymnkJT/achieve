bd3eee3234a0fc00576da50ead5a7a8dee880abe
ymnkJT <javadtomi@gmail.com>
2017-02-09 01:18:39 +0900

    Set helper method for reading status at notification

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,9 @@ module NotificationsHelper
   def posted_time(time)
     time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
   end
+
+  def read_or_unread(status)
+    return "未読" if status == false
+    return "既読"
+  end
 end
